b774c676a0eef1c8489057126d08f92d8dddae94
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-07 07:49:46 +0600

    Add class selector

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@
 const body = document.body;
 body.style.background = 'salmon';
 
-
-
-
 // //--Tag secelctor--//
 const allh1 = document.getElementsByTagName('h1');
 for (const h1 of allh1) {
@@ -16,4 +13,13 @@ for (const h1 of allh1) {
 const allh2 = document.getElementsByTagName('h2');
 for (const h2 of allh2) {
     h2.style.color = 'white';
+}
+
+// //--Class secelctor--//
+const blogs = document.getElementsByClassName('blog');
+for (const blog of blogs) {
+    blog.style.border = '4px solid black';
+    blog.style.margin = '10px';
+    blog.style.padding = '10px';
+    blog.style.borderRadius = '5px';
 }
