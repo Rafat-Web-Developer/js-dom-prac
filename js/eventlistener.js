7ae92f987945d7bdcc3618411b1fe9cf9318be2e
Rafat-Web-Developer <rafat.webdeveloper@gmail.com>
2022-02-08 07:46:28 +0600

    Add event 3nd system

--- js/eventlistener.js
+++ js/eventlistener.js
@@ -2,3 +2,9 @@
 function make_green(){
     document.body.style.backgroundColor = 'green';
 }
+
+// //--System 3--// 
+const red = document.getElementById('red');
+red.onclick = function make(){
+    document.body.style.backgroundColor='red';
+};
